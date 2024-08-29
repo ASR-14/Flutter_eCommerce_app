555e6fe3c3013c95b82d787e8253cdd73afdb119
Anh Khoa <phamvuanhkhoacv@gmail.com>
2024-08-29 14:05:16 +0700

    update

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/consts/consts.dart';
 import 'package:flutter_ecommerce/consts/lists.dart';
+import 'package:flutter_ecommerce/views/home_screen/components/featured_button.dart';
 import 'package:flutter_ecommerce/widgets_common/home_buttons.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -112,6 +113,141 @@ class HomeScreen extends StatelessWidget {
                         .size(18)
                         .fontFamily(semibold)
                         .make(),
+                  ),
+                  20.heightBox,
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(
+                        3,
+                        (index) => Column(
+                          children: [
+                            featuredButton(
+                                icon: featuredImages1[index],
+                                title: featuredTitle1[index]),
+                            10.heightBox,
+                            featuredButton(
+                                icon: featuredImages2[index],
+                                title: featuredTitle2[index]),
+                          ],
+                        ),
+                      ).toList(),
+                    ),
+                  ),
+                  20.heightBox,
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    width: double.infinity,
+                    decoration: const BoxDecoration(color: redColor),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        featuredProduct.text.white
+                            .fontFamily(bold)
+                            .size(18)
+                            .make(),
+                        10.heightBox,
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: List.generate(
+                              6,
+                              (index) => Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                    imgP1,
+                                    width: 150,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  10.heightBox,
+                                  "Laptop 4GB/64GB"
+                                      .text
+                                      .fontFamily(semibold)
+                                      .color(darkFontGrey)
+                                      .make(),
+                                  10.heightBox,
+                                  "\$600"
+                                      .text
+                                      .color(redColor)
+                                      .fontFamily(bold)
+                                      .size(16)
+                                      .make(),
+                                ],
+                              )
+                                  .box
+                                  .white
+                                  .margin(
+                                      const EdgeInsets.symmetric(horizontal: 4))
+                                  .roundedSM
+                                  .padding(const EdgeInsets.all(8))
+                                  .make(),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  10.heightBox,
+                  VxSwiper.builder(
+                    aspectRatio: 16 / 9,
+                    autoPlay: true,
+                    height: 150,
+                    enlargeCenterPage: true,
+                    itemCount: secondSliderList.length,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        secondSliderList[index],
+                        fit: BoxFit.fitWidth,
+                      )
+                          .box
+                          .rounded
+                          .clip(Clip.antiAlias)
+                          .margin(const EdgeInsets.symmetric(horizontal: 8))
+                          .make();
+                    },
+                  ),
+                  20.heightBox,
+                  GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: 6,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                      mainAxisExtent: 200,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            imgP5,
+                            width: 200,
+                            fit: BoxFit.cover,
+                          ),
+                          const Spacer(),
+                          "Laptop 4GB/64GB"
+                              .text
+                              .fontFamily(semibold)
+                              .color(darkFontGrey)
+                              .make(),
+                          10.heightBox,
+                          "\$600"
+                              .text
+                              .color(redColor)
+                              .fontFamily(bold)
+                              .size(16)
+                              .make(),
+                        ],
+                      )
+                          .box
+                          .white
+                          .rounded
+                          .margin(const EdgeInsets.symmetric(horizontal: 4))
+                          .make();
+                    },
                   )
                 ],
               ),
